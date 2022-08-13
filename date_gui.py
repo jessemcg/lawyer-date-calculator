91ac8a49f0ddc20200978d6936a98b5b4469cfbe
jessemcg <jessemcg@gmail.com>
2022-08-13 07:07:42 -0700

    message

--- date_gui.py
+++ date_gui.py
@@ -5,22 +5,24 @@ from datetime import datetime
 from datetime import timedelta
 from datetime import date
 from dateutil import relativedelta
- 
+
+# PysimpleGUI Layhout
+# Simple message at the tope
 layout = [  [sg.Text("date format = mm/dd/yyyy")], 
-
+# Buttons and input boxes
             [sg.Button('Due Date from Today', size=(30,1)), sg.Text("adding"), sg.Input(size=(3,1), key='-duefromtoday_input-'), sg.Text("days")],
-
             [sg.Button('Due Date from Diff Date', size=(30,1)), sg.Text("start date"), sg.Input(size=(10,1), key='-startdate_input-'), sg.Text("adding"), sg.Input(size=(3,1), key='-daysadded_input-'),sg.Text("days")],
-
             [sg.Button('Calculate Age', size=(30,1)), sg.Text("date of birth"), sg.Input(size=(10,1), key='-age_input-')],
-
             [sg.Button('Diff Between Two Dates', size=(30,1)), sg.Text("first date"), sg.Input(size=(10,1), key='-firstdate_input-'), sg.Text("second date"), sg.Input(size=(10,1), key='-seconddate_input-')],
-            
+# Output message      
             [sg.Text(key='-OUTPUT-')]  ]
 
-# Create the window
+# Create the main window
 window = sg.Window('Calculate Dates', layout, font="_")
 
+# Functions that do the calculations
+
+# Calculates due date from today
 def duefromtoday():
     num1 = values['-duefromtoday_input-']
     num1 = int(num1)
@@ -29,7 +31,8 @@ def duefromtoday():
     Enddate_formatted = Enddate.strftime("%B %d, %Y") 
     values['-duedate-'] = Enddate_formatted
     return None
-    
+
+# Calculates due date from a different date
 def duefromdiff():
     Begindatestring = values['-startdate_input-']
     num1 = values['-daysadded_input-']
@@ -40,8 +43,8 @@ def duefromdiff():
     values['-duedate-'] = Enddate_formatted
     return None
 
+# Calculates someone age if date of birth provided
 def age():
-    
     birthDate = values['-age_input-']
     birthDate = datetime.strptime(birthDate, "%m/%d/%Y").date()
     currentDate = datetime.today().date()
@@ -57,7 +60,8 @@ def age():
         age = age-1
     values['-duedate-'] = age
     return None
-  
+
+# Calculates the time between two dates
 def diff():
     str_d1 = values['-firstdate_input-']
     str_d2 = values['-seconddate_input-']
@@ -67,7 +71,7 @@ def diff():
     values['-duedate-'] = f"{delta.years} years, {delta.months} months, and {delta.days} days"
     return None
     
-# Calling each function with an if statement tied to their submit button
+# Calling each function with an if statement tied to their pysimpple gui submit button
 while True:
     event, values = window.read()
     if event == 'Due Date from Today':
@@ -81,5 +85,5 @@ while True:
     # See if user wants to quit or window was closed
     if event == sg.WINDOW_CLOSED or event == 'Close':
         break
-    # Output a message to the window
+    # Output the answer in large yellow font
     window['-OUTPUT-'].update(values['-duedate-'], text_color='yellow', font='Courier 20')
